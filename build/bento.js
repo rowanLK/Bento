4ae4eea30fb312614d80b530e2ad091c102a3d60
Hernan Zhou <hernanzh@gmail.com>
2015-12-05 17:49:49 +0100

    reload modules

--- build/bento.js
+++ build/bento.js
@@ -2942,10 +2942,31 @@ var requirejs, require, define;
  */
 (function () {
     'use strict';
-    var bento = {
-        require: window.require,
-        define: window.define
-    };
+    var startWatching = false,
+        modules = [],
+        rjs = window.requirejs, // cache requirejs
+        req = window.require, // cache requirejs
+        def = window.define, // cache requirejs
+        bento = {
+            require: req,
+            define: function () {
+                var name = arguments[0];
+                if (startWatching) {
+                    modules.push(name);
+                }
+                def.apply(this, arguments);
+            },
+            refresh: function () {
+            	var i = 0;
+                // undefines every module loaded since watch started 
+                for (i = 0; i < modules.length; ++i) {
+                	rjs.undef(modules[i]);
+                }
+            },
+            watch: function () {
+                startWatching = true;
+            }
+        };
     window.bento = window.bento || bento;
 }());
 /*
