469059d0f3c7c07da53541820a9f3f2e25ce6100
Hernan Zhou <hernanzh@gmail.com>
2016-02-11 15:40:25 +0100

    clickbutton check for inactive frame

--- build/bento.js
+++ build/bento.js
@@ -12841,24 +12841,25 @@ bento.define('bento/gui/clickbutton', [
     return function (settings) {
         var viewport = Bento.getViewport(),
             active = true,
+            animations = settings.animations || {
+                'up': {
+                    speed: 0,
+                    frames: [0]
+                },
+                'down': {
+                    speed: 0,
+                    frames: [1]
+                },
+                'inactive': {
+                    speed: 0,
+                    frames: [2]
+                }
+            },
             sprite = new Sprite({
                 image: settings.image,
                 frameWidth: settings.frameWidth || 32,
                 frameHeight: settings.frameHeight || 32,
-                animations: settings.animations || {
-                    'up': {
-                        speed: 0,
-                        frames: [0]
-                    },
-                    'down': {
-                        speed: 0,
-                        frames: [1]
-                    },
-                    'inactive': {
-                        speed: 0,
-                        frames: [2]
-                    }
-                }
+                animations: animations
             }),
             clickable = new Clickable({
                 onClick: function () {
@@ -12919,7 +12920,7 @@ bento.define('bento/gui/clickbutton', [
                 ],
                 family: ['buttons'],
                 init: function () {
-                    if (!active) {
+                    if (!active && animations.inactive) {
                         sprite.animation.setAnimation('inactive');
                     } else {
                         sprite.animation.setAnimation('up');
@@ -12929,7 +12930,7 @@ bento.define('bento/gui/clickbutton', [
             entity = new Entity(entitySettings).extend({
                 setActive: function (bool) {
                     active = bool;
-                    if (!active) {
+                    if (!active && animations.inactive) {
                         sprite.animation.setAnimation('inactive');
                     } else {
                         sprite.animation.setAnimation('up');
--- js/modules/gui/clickbutton.js
+++ js/modules/gui/clickbutton.js
@@ -23,24 +23,25 @@ bento.define('bento/gui/clickbutton', [
     return function (settings) {
         var viewport = Bento.getViewport(),
             active = true,
+            animations = settings.animations || {
+                'up': {
+                    speed: 0,
+                    frames: [0]
+                },
+                'down': {
+                    speed: 0,
+                    frames: [1]
+                },
+                'inactive': {
+                    speed: 0,
+                    frames: [2]
+                }
+            },
             sprite = new Sprite({
                 image: settings.image,
                 frameWidth: settings.frameWidth || 32,
                 frameHeight: settings.frameHeight || 32,
-                animations: settings.animations || {
-                    'up': {
-                        speed: 0,
-                        frames: [0]
-                    },
-                    'down': {
-                        speed: 0,
-                        frames: [1]
-                    },
-                    'inactive': {
-                        speed: 0,
-                        frames: [2]
-                    }
-                }
+                animations: animations
             }),
             clickable = new Clickable({
                 onClick: function () {
@@ -101,7 +102,7 @@ bento.define('bento/gui/clickbutton', [
                 ],
                 family: ['buttons'],
                 init: function () {
-                    if (!active) {
+                    if (!active && animations.inactive) {
                         sprite.animation.setAnimation('inactive');
                     } else {
                         sprite.animation.setAnimation('up');
@@ -111,7 +112,7 @@ bento.define('bento/gui/clickbutton', [
             entity = new Entity(entitySettings).extend({
                 setActive: function (bool) {
                     active = bool;
-                    if (!active) {
+                    if (!active && animations.inactive) {
                         sprite.animation.setAnimation('inactive');
                     } else {
                         sprite.animation.setAnimation('up');
