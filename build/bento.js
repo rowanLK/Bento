996a3e1568d9af8326340c4431744e3dd12620a9
Hernan Zhou <hernanzh@gmail.com>
2018-03-14 13:05:49 +0100

    create a build

--- build/bento.js
+++ build/bento.js
@@ -2127,6 +2127,7 @@ var requirejs, require, define;
     window.define = undefined;
 }());
 if (!bento) {
+    // if bento still isn't defined at this point, then window isn't the global object
     var bento = window.bento;
 }
 /*
@@ -3104,8 +3105,11 @@ bento.define('bento', [
             clientWidth = innerWidth;
             clientHeight = innerWidth * canvasRatio;
         }
-        canvas.style.width = clientWidth + 'px';
-        canvas.style.height = clientHeight + 'px';
+        if (canvas.style) {
+            // browser specific
+            canvas.style.width = clientWidth + 'px';
+            canvas.style.height = clientHeight + 'px';
+        }
 
         canvasScale.x = clientWidth / viewport.width;
         canvasScale.y = clientHeight / viewport.height;
@@ -6523,6 +6527,91 @@ bento.define('bento/components/clickable', [
     return Clickable;
 });
 /**
+ * Component that listens to an event fired by the EventSystem.
+ * Automatically stops listening if the entity is destroyed or if the component is removed
+ * <br>Exports: Constructor
+ * @module bento/components/eventlistener
+ * @moduleName EventListener
+ * @snippet EventListener.snippet
+EventListener({
+    name: '${1:eventListener}',
+    eventName: '${2:eventName}',
+    ignorePause: ${3:false},
+    onEvent: function (data) {
+        $4
+    }
+})
+ * @param {Object} settings - Settings
+ * @param {String} settings.name - Component name, defaults to 'eventListener'
+ * @param {String} settings.eventName - Event name to listen to
+ * @param {Boolean} settings.ignorePause - Listen to events even if entity is paused
+ * @param {Function} settings.onEvent - Event callback
+ */
+bento.define('bento/components/eventlistener', [
+    'bento',
+    'bento/math/vector2',
+    'bento/math/rectangle',
+    'bento/entity',
+    'bento/eventsystem',
+    'bento/utils',
+    'bento/tween'
+], function (
+    Bento,
+    Vector2,
+    Rectangle,
+    Entity,
+    EventSystem,
+    Utils,
+    Tween
+) {
+    'use strict';
+    var isPaused = function (entity) {
+        var rootPause = 0;
+        if (!Bento.objects || !entity) {
+            return false;
+        }
+        rootPause = entity.updateWhenPaused;
+        // find root parent
+        while (entity.parent) {
+            entity = entity.parent;
+            rootPause = entity.updateWhenPaused;
+        }
+
+        return rootPause < Bento.objects.isPaused();
+    };
+    return function (settings) {
+        var viewport = Bento.getViewport();
+        var componentName = settings.name || 'eventListener';
+        var eventName = settings.eventName;
+        var ignorePause = settings.ignorePause || false;
+        var onEvent = settings.callback || settings.onEvent || function () {};
+        var entity;
+        var component = {
+            name: componentName,
+            start: function (data) {
+                if (!eventName) {
+                    Utils.log('WARNING: eventName is not defined! Using component name as event name');
+                    eventName = componentName;
+                }
+                EventSystem.on(eventName, ignorePause ? onEvent : wrapperCallback);
+            },
+            destroy: function (data) {
+                EventSystem.off(eventName, ignorePause ? onEvent : wrapperCallback);
+            },
+            attached: function (data) {
+                entity = data.entity;
+            }
+        };
+        // this callback is used when event listener can pause
+        var wrapperCallback = function (data) {
+            if (!isPaused(entity)) {
+                onEvent(data);
+            }
+        };
+        return component;
+    };
+});
+/**
  * Component that fills a square.
  * <br>Exports: Constructor
  * @module bento/components/fill
@@ -6586,7 +6675,18 @@ bento.define('bento/components/fill', [
  * Component for modal popups - pauses the game on start and resets on destroy
  * The parent entity will not be paused. Pauselevels will stack when more entities with the
  * modal component are attached to the game.
+ * <br>Exports: Constructor
+ * @module bento/components/modal
  * @moduleName Modal
+ * @snippet Modal|constructor
+Modal({})
+ * @snippet Modal|target
+Modal({
+    pauseLevel: ${1:1}
+})
+ * @param {Object} settings - Settings
+ * @param {String} [settings.pauseLevel] - Target pause level, recommended to ignore this parameter and 
+ * let the component set the automatic pause level automatically. 
  */
 bento.define('components/modal', [
     'bento',
@@ -6612,7 +6712,7 @@ bento.define('components/modal', [
         var oldPauseLevel;
         var component = {
             name: 'modal',
-            /**
+            /*
              * Current pauseLevel
              */
             pauseLevel: 0,
@@ -8481,7 +8581,6 @@ bento.define('bento/managers/asset', [
             // cocoon lazy load, might be useful some day?
             // img.cocoonLazyLoad = true;
 
-            img.src = source;
             img.addEventListener('load', function () {
                 callback(null, name, img);
             }, false);
@@ -8489,6 +8588,8 @@ bento.define('bento/managers/asset', [
                 // TODO: Implement failure: should it retry to load the image?
                 Utils.log('ERROR: loading image ' + source);
             }, false);
+
+            img.src = source;
         };
         var loadTTF = function (name, source, callback) {
             // for every font to load we measure the width on a canvas
